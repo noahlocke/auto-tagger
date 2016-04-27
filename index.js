1adf8c341c7319ac48416bd9e411913d6ec538c0
Locke Noah J <nlocke@uwhealth.org>
2016-04-27 09:23:40 -0500

    listen for webhook

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var ShopifyAPI = require('shopify-node-api');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,11 +11,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/auto-tagger', function (request, response) {
+	console.log(request);
+});
+
+
+var Shopify = new shopifyAPI({
+	shop: 'kwee-jack-fish-co-llc', // MYSHOP.myshopify.com 
+	shopify_api_key: '78987fdb8ac036c581546ad56512140c', // Your API key 
+	access_token: '071455328b55cc0a95d08059ac882f65' // Your API password 
 });
 
 
